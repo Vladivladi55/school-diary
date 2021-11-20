35a2f28bd59082c2350c2575574e06c439e2f9aa
Vladivladi55 <86762559+Vladivladi55@users.noreply.github.com>
2021-11-20 13:45:43 +0200

    Add files via upload

---
+++ struct/routing/noterouting.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const Student = require('../database/student');
+
+app.post('/notes', (req, res)=>{
+    
+})
+
+
+module.exports = router;
